7741c49fddd2cbfa02c13061c58a50bcfef5d84e
piyusshh07 <piyushdeshmane2003@gmail.com>
2024-02-14 10:35:19 +0530

    db updated and error fixed

--- app/src/main/java/com/project/moneymind/DBHelper.java
+++ app/src/main/java/com/project/moneymind/DBHelper.java
@@ -1,11 +1,9 @@
 package com.project.moneymind;
-
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
