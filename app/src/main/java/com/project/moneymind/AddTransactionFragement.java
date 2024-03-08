7aba5bca0ee6dcb242dbb83a0e52dd81103e50f9
piyusshh07 <piyushdeshmane2003@gmail.com>
2024-03-08 16:32:27 +0530

    fab displayed income and expense transaction can be typed

--- app/src/main/java/com/project/moneymind/AddTransactionFragement.java
+++ app/src/main/java/com/project/moneymind/AddTransactionFragement.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.project.moneymind.databinding.FragmentAddTransactionFragementBinding;
@@ -22,8 +23,10 @@ public class AddTransactionFragement extends BottomSheetDialogFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
     }
     FragmentAddTransactionFragementBinding binding;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,6 +34,24 @@ public class AddTransactionFragement extends BottomSheetDialogFragment {
         // Inflate the layout for this fragment
         binding= FragmentAddTransactionFragementBinding.inflate(inflater);
 
+
+        binding.incomeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.incomeBtn.setBackground(getContext().getDrawable(R.drawable.default_income_selector));
+                binding.expenseBtn.setBackground(getContext().getDrawable(R.drawable.default_selector));
+            }
+        });
+
+        binding.expenseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.expenseBtn.setBackground(getContext().getDrawable(R.drawable.default_expense_selector));
+                binding.incomeBtn.setBackground(getContext().getDrawable(R.drawable.default_selector));
+            }
+        });
         return binding.getRoot();
+
     }
+
 }
